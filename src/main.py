4976c12b4b766539182d8a80b069c1eb2674ba38
luizpassonidev <luizpassonidev@gmail.com>
2026-08-30 11:46:07 -0300

    /new and personality

--- src/main.py
+++ src/main.py
@@ -3,11 +3,28 @@ import os
 from dotenv import load_dotenv
 from google import genai
 
+personality = """
+Você é o Bash AI, um assistente de inteligência artificial executado no terminal.
+
+Seu nome é Bash AI.
+Você responde sempre em português do Brasil.
+
+Características:
+- Seja amigável e natural.
+- Seja objetivo, mas explique quando necessário.
+- Quando explicar código, use exemplos simples e claros.
+- Não invente informações. Quando não souber algo, diga que não sabe.
+- Evite respostas excessivamente longas quando uma resposta curta for suficiente.
+"""
+
+
+
 #tela inicial
 print("\t    Welcome to Bash AI!")
 from funcoes import logo
 logo()
 time.sleep(2)
+
 os.system("clear    ")
 
 
@@ -19,7 +36,10 @@ api=os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=api)
 
 chat = client.chats.create(
-    model="gemini-3.5-flash-lite"
+    model="gemini-3.5-flash-lite",
+    config={
+        "system_instruction": personality
+    }
 )
 
 #chat 
@@ -44,6 +64,17 @@ while True:
         if msg == "/help":
             print("Bash AI: Lista de Comandos do Bash AI:\n/help - Mostra Lista de comandos.\n/clear - Limpa a tela do Bash AI.\n/exit - Sai da conversa atual.\n/model - Mostra o modelo que o Bash AI está utilizando para gerar texto.")
             aux+=1
+        if msg == "/new":
+            os.system("clear")
+            chat = client.chats.create(
+                model="gemini-3.5-flash-lite",
+                config={
+                        "system_instruction": personality
+                    }
+            )
+            flag = 0
+            aux+=1
+            continue
         if aux == 0:
             resp = chat.send_message(
                 message= f"Bash AI:{msg}")
